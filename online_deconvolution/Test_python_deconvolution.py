acb82caccfcda661a0ff164ea619a1a04930e4d3
JB <fiche@cbs.cnrs.fr>
2022-01-24 16:50:58 +0100

    Test

--- online_deconvolution/Test_python_deconvolution.py
+++ online_deconvolution/Test_python_deconvolution.py
@@ -4,17 +4,19 @@ from glob import glob
 data_folder = "/mnt/grey/DATA/users/JB/test_HiM/test_data_Marion"
 dest_folder = "/mnt/grey/DATA/users/JB/test_HiM/test_data_Marion/Deconvolved"
 
+n_channel = 2
+
 
 # to avoid pop up warning
 huOpt.verb(mode="silent")
 
 # look for all the tif files
 path_files = glob(data_folder + '/**/*.tif', recursive=True)
-print(f'Number of tif files found : {len(path_files)}')
+print(f'Number of tif files found : {len(path_files)} \n')
 
 # for each tif file, open an image class
 for n, path in enumerate(path_files):
-    print(f'Loading image #{n} with the following path : {path}')
+    print(f'Loading image #{n} with the following path : {path} \n')
     raw = image(path=path)
 
 print("DONE!")
