1fa160d2f4c5e7901d6293176b686c3bffba216b
JB <fiche@cbs.cnrs.fr>
2022-01-24 18:00:28 +0100

    Test deconvolution options

--- online_deconvolution/Test_python_deconvolution.py
+++ online_deconvolution/Test_python_deconvolution.py
@@ -11,7 +11,7 @@ huOpt.verb(mode="silent")
 
 # look for all the tif files
 path_files = glob(data_folder + '/**/*.tif', recursive=True)
-print(f'Number of tif files found : {len(path_files)} \n')
+# print(f'Number of tif files found : {len(path_files)} \n')
 huOpt.report("Number of tif files found : ${len(path_files)}")
 
 # create an empty image, so the deconvolution is done using a theoretical psf based on Microscopic Parameters
@@ -21,6 +21,7 @@ psf = image()
 for n, path in enumerate(path_files):
     # print(f'Loading image #{n} with the following path : {path} \n')
     raw = image(path=path)
-    result_img = raw.cmle(psf, it=2, snr=2, q=0.01, bgMode="manual", bg=[0])
+    result_img = raw.cmle(psf, it=5, bgMode="wf", bgRadius=0.7, blMode=off)
 
+huOpt.report("Done!")
 exit()
