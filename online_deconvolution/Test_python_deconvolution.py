dc3a51de9a3f168dfb0c62afab6dec0317dc9296
JB <fiche@cbs.cnrs.fr>
2022-01-24 16:23:27 +0100

    Test loading image from path

--- online_deconvolution/Test_python_deconvolution.py
+++ online_deconvolution/Test_python_deconvolution.py
@@ -6,11 +6,15 @@ dest_folder = "/mnt/grey/DATA/users/JB/test_HiM/test_data_Marion/Deconvolved"
 
 
 # to avoid pop up warning
-huOpt.verb(mode = "silent")
+huOpt.verb(mode="silent")
 
 # look for all the tif files
-files = glob(data_folder + '/**/*.tif', recursive=True)
-print(files)
+path_files = glob(data_folder + '/**/*.tif', recursive=True)
+print(f'Number of tif files found : {len(files)}')
 
-# raw = image(path=)
+# for each tif file, open an image class
+for n, path in enumerate(path_files):
+    print(f'Loading image #{n} with the following path : {path}')
+    raw = image(path=path)
+
 print("DONE!")
