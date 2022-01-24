6b0122e3141b00e53d52a49c7ce6285e6f94198b
JB <fiche@cbs.cnrs.fr>
2022-01-24 17:22:21 +0100

    Add deconvolution operation

--- online_deconvolution/Test_python_deconvolution.py
+++ online_deconvolution/Test_python_deconvolution.py
@@ -7,15 +7,19 @@ dest_folder = "/mnt/grey/DATA/users/JB/test_HiM/test_data_Marion/Deconvolved"
 n_channel = 2
 
 # to avoid pop up warning
-# huOpt.verb(mode="silent")
+huOpt.verb(mode="silent")
 
 # look for all the tif files
 path_files = glob(data_folder + '/**/*.tif', recursive=True)
 # print(f'Number of tif files found : {len(path_files)} \n')
 
+# create an empty image, so the deconvolution is done using a theoretical psf based on Microscopic Parameters
+psf = image()
+
 # for each tif file, open an image class
 for n, path in enumerate(path_files):
     # print(f'Loading image #{n} with the following path : {path} \n')
     raw = image(path=path)
+    result_img = raw.cmle(psf, it=2, snr=2, q=0.01, bgMode="manual", bg=[0])
 
-print("DONE!")
+exit()
