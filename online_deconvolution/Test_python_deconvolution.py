b7e1068f6f28378f0bdf3cea0a324ad473903a31
JB <fiche@cbs.cnrs.fr>
2022-01-24 13:50:48 +0100

    create image

--- online_deconvolution/Test_python_deconvolution.py
+++ online_deconvolution/Test_python_deconvolution.py
@@ -1,1 +1,5 @@
-print("Hello world")
+data_folder = "/mnt/grey/DATA/users/JB/test_HiM/test_data_Marion"
+dest_folder = "/mnt/grey/DATA/users/JB/test_HiM/test_data_Marion/Deconvolved"
+
+raw = image(dim=[2048, 2048, 60], chan=2)
+print("DONE!")
