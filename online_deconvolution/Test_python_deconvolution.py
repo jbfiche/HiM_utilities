c7877b9c33b4a61afd9cb77bff4e6a0a5f6871e4
JB <fiche@cbs.cnrs.fr>
2022-01-24 18:24:02 +0100

    test

--- online_deconvolution/Test_python_deconvolution.py
+++ online_deconvolution/Test_python_deconvolution.py
@@ -26,7 +26,7 @@ for n, path in enumerate(path_files):
     file_name = os.path.basename(path)
     new_file_name = os.path.splitext(file_name)[0] + "_decon.tif"
     saving_path = os.path.join(dest_folder, new_file_name)
-    result_img.save(saving_path, type="singletiff", tiffMultiDir="on")
+    result_img.save(saving_path, tiffMultiDir="on")
 
 huOpt.report("Done!")
 exit()
