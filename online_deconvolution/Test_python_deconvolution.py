fb453665b567dc50b03736fa6edf064331ea4491
JB <fiche@cbs.cnrs.fr>
2022-01-24 16:13:57 +0100

    recursive search

--- online_deconvolution/Test_python_deconvolution.py
+++ online_deconvolution/Test_python_deconvolution.py
@@ -1,5 +1,16 @@
+import os
+from glob import glob
+
 data_folder = "/mnt/grey/DATA/users/JB/test_HiM/test_data_Marion"
 dest_folder = "/mnt/grey/DATA/users/JB/test_HiM/test_data_Marion/Deconvolved"
 
-raw = image(dim=[2048, 2048, 60], chan=2)
+
+# to avoid pop up warning
+huOpt.verb(mode = "silent")
+
+# look for all the tif files
+files = glob.glob(data_folder + '/**/**/*.tif', recursive=True)
+print(files)
+
+# raw = image(path=)
 print("DONE!")
