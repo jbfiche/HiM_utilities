4deebcd95a604d7e73d1988f4be78a5f228b15ae
JB <fiche@cbs.cnrs.fr>
2022-02-23 10:51:03 +0100

    Update image parameters

--- online_deconvolution/Test_python_deconvolution.py
+++ online_deconvolution/Test_python_deconvolution.py
@@ -61,8 +61,9 @@ for n, path in enumerate(path_files):
 
         # perform deconvolution
         im_param = raw_channel.setp(na=NA, objQuality=objQuality, ril=ri_medium, ri=ri_sample, ex=wavelength_excitation[0],
-                         em=wavelength_emission[0], baseline=100, dx=0.106, dy=0.106, dz=0.25, mag=60.0, offZ=5,
-                         imagingDir="upward", micr=microscope, tclReturn=True)
+                                    em=wavelength_emission[0], baseline=100, dx=dX*voxel_size[0]/1000,
+                                    dy=dY*voxel_size[1]/1000, dz=n_frames*voxel_size[2]/1000, mag=60.0, offZ=5,
+                                    imagingDir="upward", micr=microscope, tclReturn=True)
         text_report = f'image parameters : {im_param}'
         huOpt.report(text_report)
 
